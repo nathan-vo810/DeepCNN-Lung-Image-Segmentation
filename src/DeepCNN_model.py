826474bf28eb0a495864265ee51becce6090aefe
Anh Vo Nguyen Nhat <vnnanh@aavn.local>
2018-06-13 10:29:40 +0700

    Add plot function

--- src/DeepCNN_model.py
+++ src/DeepCNN_model.py
@@ -4,7 +4,7 @@ from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint
 
 from src import DataLoader
-
+import matplotlib.pyplot as plt
 import os
 
 DIR = os.path.dirname(__file__)
@@ -63,10 +63,31 @@ class DeepCNN:
         # print("Weight loaded.")
         print("Training...")
         model_checkpoint = ModelCheckpoint(WEIGHT_PATH, monitor='loss', verbose=1, save_best_only=True)
-        model.fit(train_data, label_data, batch_size=128, epochs=20, verbose=1, validation_split=0.3, shuffle=True,
+        train_history = model.fit(train_data, label_data, batch_size=128, epochs=20, verbose=1, validation_split=0.3, shuffle=True,
                   callbacks=[model_checkpoint])
+        return train_history
+
+    def plot_diagram(self, history):
+        # Loss Curves
+        plt.figure(figsize=[8,6])
+        plt.plot(history.history['loss'],'r',linewidth=3.0)
+        plt.plot(history.history['val_loss'],'b',linewidth=3.0)
+        plt.legend(['Training loss', 'Validation Loss'],fontsize=18)
+        plt.xlabel('Epochs ',fontsize=16)
+        plt.ylabel('Loss',fontsize=16)
+        plt.title('Loss Curves',fontsize=16)
+        
+        # Accuracy Curves
+        plt.figure(figsize=[8,6])
+        plt.plot(history.history['acc'],'r',linewidth=3.0)
+        plt.plot(history.history['val_acc'],'b',linewidth=3.0)
+        plt.legend(['Training Accuracy', 'Validation Accuracy'],fontsize=18)
+        plt.xlabel('Epochs ',fontsize=16)
+        plt.ylabel('Accuracy',fontsize=16)
+        plt.title('Accuracy Curves',fontsize=16)
 
 
 if __name__ == '__main__':
     network = DeepCNN()
-    network.train()
+    train_history = network.train()
+    network.plot_diagram(train_history)
